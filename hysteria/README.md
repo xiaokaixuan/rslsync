de6714a1e1292ec5f0bd306fb0da95f67d05cba9
xiaokaixuan <xiaokaixuan@gmail.com>
2023-09-27 13:38:42 +0800

    Add Hysteria2

---
+++ hysteria/README.md
@@ -0,0 +1,12 @@
+## Hysteria 2
+
+### Usage
+
+```bash
+docker run -d --name hysteria \
+    --restart unless-stopped -p4433:443/udp xiaokaixuan/hysteria
+```
+
+### Docker Hub
+> *https://hub.docker.com/r/xiaokaixuan/hysteria*
+
---
+++ hysteria/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+cd /hysteria
+
+cat<<-EOF>config.yaml
+listen: :${PORT}
+
+tls:
+  cert: cert.pem
+  key: key.pem
+
+auth:
+  type: password
+  password: ${PASSWORD}
+
+masquerade: 
+  type: proxy
+  proxy:
+    url: https://www.bing.com 
+    rewriteHost: true
+EOF
+
+exec /hysteria/hysteria-linux-amd64-avx server
+
